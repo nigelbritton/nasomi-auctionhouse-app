1c30183dc787fed5e194c9d5e965af4ccfadac94
Nigel Complete Control <nigel@completecontrol.co.uk>
2018-07-23 14:03:04 +0100

    Init. Commit

---
+++ routes/api.js
@@ -0,0 +1,34 @@
+/**
+ * Created by Nigel.Britton on 23/07/2018.
+ */
+
+'use strict';
+
+var express = require('express'),
+    router = express.Router();
+
+var loadContent = require('../lib/loadContent')();
+
+router.post('/searchCharByName', function(req, res, next) {
+    loadContent.searchCharByName( req.body.charname )
+        .then(function (response) {
+            res.send(response);
+        });
+});
+
+router.post('/searchChar', function(req, res, next) {
+    loadContent.searchChar( req.body.charid )
+        .then(function (response) {
+            res.send(response);
+        });
+});
+
+router.post('/searchItemByName', function(req, res, next) {
+    res.json({});
+});
+
+router.post('/searchItem', function(req, res, next) {
+    res.json({});
+});
+
+module.exports = router;
